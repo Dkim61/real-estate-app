81ac0e98538bb0b671cb0ea4bae179091edd8c0e
dkim61 <dkim187@binghamton.edu>
2022-02-03 20:35:51 -0500

    contacts

--- backend/settings.py
+++ backend/settings.py
@@ -99,7 +99,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'djk4696@gmail.com'
-EMAIL_HOST_PASSWORD = 'csyzafydhnukbmzm'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 # Password validation
