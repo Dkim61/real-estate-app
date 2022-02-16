bcb3c7a3255de5d34af1ebfb4156b0bc22a86ccc
dkim61 <dkim187@binghamton.edu>
2022-02-16 00:32:03 -0500

    listingForm

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const card = () => {
+    <div>
+        card
+    </div>
+};
+
+export default card;
+
---
+++ frontend/src/components/Pagination.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const pagination = () => {
+    <div>
+        pagination
+    </div>
+};
+
+export default pagination;
+
